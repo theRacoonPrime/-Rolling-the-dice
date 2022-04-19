c3cbf619316e7e6c99621467adb975023b9a4295
theRacoonPrime <98499546+theRacoonPrime@users.noreply.github.com>
2022-04-19 08:41:54 +0200

    Add files via upload

---
+++ rolling the dice.py
@@ -0,0 +1,8 @@
+import random
+
+roll_dice = input("Write start to dice roll: ")
+
+if roll_dice == "start":
+   posiblle_results = [1, 2, 3, 4, 5, 6]
+   result = random.choice(posiblle_results)
+   print("Result of dice rolling is : " + str(result))
